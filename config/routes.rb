239373ffb357832407540061f2128a96a58c38bc
Michael Boitin <michaelboitin@MacBook-Pro-de-Michael-2.local>
2016-09-09 15:45:32 +0200

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :microposts
+  resources :users
 
-  root 'application#hello'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+
 end
